441ea5000b3cf06df2180bf60984884825b5ee74
Juan Camilo <jcamilo596@gmail.com>
2023-12-15 00:16:19 -0300

    animação das gemas

--- SunnyLand/Assets/Scripts/Classes/Items/Gem.cs
+++ SunnyLand/Assets/Scripts/Classes/Items/Gem.cs
@@ -3,6 +3,18 @@ using System;
 
 public class Gem : MonoBehaviour, ICollectible {
     public static event Action OnGemCollected;
+    private readonly float amplitude = 0.25f;
+    private readonly float speed = 2.5f;
+    private Vector2 startPos;
+
+    void Awake() {
+        startPos = transform.position;
+    }
+
+    void Update() {
+        float newY = startPos.y + amplitude * Mathf.Sin(speed * Time.time);
+        transform.position = new Vector2(startPos.x, newY);
+    }
 
     public void Collect() {
         Debug.Log("Gema Coletada!");
