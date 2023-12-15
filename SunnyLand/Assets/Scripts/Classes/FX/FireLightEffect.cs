3aa4158f71ec8996be037052444248cf673ab827
Juan Camilo <jcamilo596@gmail.com>
2023-12-15 01:01:36 -0300

    Efeito de iluminação de fogo.

---
+++ SunnyLand/Assets/Scripts/Classes/FX/FireLightEffect.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class FireLightEffect : MonoBehaviour {
+    private Light2D fireLight;
+    [SerializeField] private float intensity = 1.0f;
+    [SerializeField] private float randomIntensityRange = 0.25f;
+    [SerializeField] private float speed = 0.5f;
+    private float originalIntensity;
+    private float randomOffset;
+
+    void Start() {
+        fireLight = GetComponent<Light2D>();
+        originalIntensity = fireLight.intensity;
+        randomOffset = Random.Range(0f, 1f);
+    }
+
+    void Update() {
+        float randomIntensity = Random.Range(-randomIntensityRange, randomIntensityRange);
+        float ocillation = Mathf.Sin((Time.time + randomOffset) * speed);
+        float newIntensity = originalIntensity + ocillation * intensity * randomIntensity;
+
+        fireLight.intensity = Mathf.Max(0, newIntensity);
+    }
+}
